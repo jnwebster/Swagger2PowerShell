ab99329a4a6c63cbb92d72abc0e867895e4dac87
jnwebster <j.n.webster@gmail.com>
2015-03-12 15:22:52 -0500

    Spec retrieved and deserialized

---
+++ Swagger2PowerShell/Swagger2PowerShell.Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Swagger2PowerShell
+{
+    [TestClass]
+    public class Swagger2PowerShellTest
+    {
+        [TestCategory("Integration")]
+        [TestMethod]
+        public void ShouldDoSomething()
+        {
+            var response = Swagger2PowerShell.CreatePowerShellModuleFromSwaggerSpec("http://petstore.swagger.io/v2/swagger.json", "SPS", "C:\\Temp\\");
+            Assert.IsTrue(response);
+        }
+    }
+}
