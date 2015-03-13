8f99b5e38aed5ed90211e5b541765743bcd93a55
jnwebster <j.n.webster@gmail.com>
2015-03-13 07:55:40 -0500

    Splitting the unit tests out

--- Swagger2PowerShell.Test/Swagger2PowerShell.Test.cs
+++ Swagger2PowerShell.Test/Swagger2PowerShell.Test.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Swagger2PowerShell
+namespace Swagger2PowerShellTest
 {
     [TestClass]
     public class Swagger2PowerShellTest
@@ -14,7 +14,7 @@ namespace Swagger2PowerShell
         [TestMethod]
         public void ShouldDoSomething()
         {
-            var response = Swagger2PowerShell.CreatePowerShellModuleFromSwaggerSpec("http://webster-dev2014.madvdi.local:1234/api-docs/", "S2P", "C:\\Temp\\vWorkspace-WebAPI.ps1");
+            var response = Swagger2PowerShell.Swagger2PowerShell.CreatePowerShellModuleFromSwaggerSpec("http://webster-dev2014.madvdi.local:1234/api-docs/", "S2P", "C:\\Temp\\vWorkspace-WebAPI.ps1");
             Assert.IsTrue(response);
         }
     }
